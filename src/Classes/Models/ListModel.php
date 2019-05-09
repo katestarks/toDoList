1f07dd883305bfb2dbc34d4c117e7f72148288a2
katestarks <katestarks@me.com>
2019-05-09 11:19:00 +0100

    created listModel

---
+++ src/Classes/Models/ListModel.php
@@ -0,0 +1,17 @@
+<?php
+
+class ListModel {
+
+    $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function viewList() {
+        $query = $this->db->prepare("SELECT `tasks` FROM `taskList` WHERE `completed` = 0");
+        $query->execute();
+        return $query->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
