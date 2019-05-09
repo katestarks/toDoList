df4da8a1af3154f234a558617676386919bff849
katestarks <katestarks@me.com>
2019-05-09 15:17:00 +0100

    updated list model to capture id and update with status

--- src/Classes/Models/ListModel.php
+++ src/Classes/Models/ListModel.php
@@ -12,7 +12,7 @@ class ListModel {
     }
 
     public function viewList() {
-        $query = $this->db->prepare("SELECT `tasks` FROM `taskList` WHERE `completed` = 0");
+        $query = $this->db->prepare("SELECT `id`,`tasks` FROM `taskList` WHERE `completed` = 0");
         $query->execute();
         return $query->fetchAll(\PDO::FETCH_ASSOC);
     }
@@ -20,6 +20,12 @@ class ListModel {
     public function addTask($task) {
         $query = $this->db->prepare("INSERT INTO `taskList` (`tasks`) VALUES (:task)");
         $query->bindParam(':task', $task);
+        return $query->execute();
+    }
+
+    public function addCompletedFlag($id) {
+        $query = $this->db->prepare("UPDATE `taskList` SET `completed` = 1 WHERE `id` = :id");
+        $query->bindParam(':id', $id);
         return $query->execute();
     }
 }
