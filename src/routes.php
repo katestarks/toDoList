c96c2cf5da2cabc0403203097f1d26754e4a261f
katestarks <katestarks@me.com>
2019-05-09 11:49:56 +0100

    completed route

--- src/routes.php
+++ src/routes.php
@@ -7,6 +7,6 @@ use Slim\Http\Response;
 return function (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/', //TODO complete route);
+    $app->get('/', 'ViewListController');
 
 };
