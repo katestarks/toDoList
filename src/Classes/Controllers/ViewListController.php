483dbaf404ccdfcec7ffaab320b3a5c36c8a70c8
katestarks <katestarks@me.com>
2019-05-09 11:33:24 +0100

    added constructor and methods to ViewListController

--- src/Classes/Controllers/ViewListController.php
+++ src/Classes/Controllers/ViewListController.php
@@ -1,6 +1,24 @@
 <?php
 
+namespace Portal\Controllers;
+
+use Portal\Models\ListModel;
+use Slim\Views\PhpRenderer;
+
 class ViewListController {
 
-    public $
+    public $renderer;
+    public $listModel;
+
+    public function __construct(PhpRenderer $renderer, ListModel $listModel)
+    {
+        $this->renderer = $renderer;
+        $this->listModel = $listModel;
+    }
+
+    public function __invoke($request, $response, $args)
+    {
+        $args['tasks'] = $this->listModel->viewList();
+        $this->renderer->($response, $request, $args);
+    }
 }
