dce724c32048c8c6fd91be8220e4af1dda1e9c67
katestarks <katestarks@me.com>
2019-05-10 11:54:16 +0100

    added RandomTaskControllerFactory to dependencies

--- src/dependencies.php
+++ src/dependencies.php
@@ -33,4 +33,5 @@ return function (App $app) {
     $container['ViewListController'] = new \Portal\Factories\ViewListControllerFactory();
     $container['AddTaskController'] = new \Portal\Factories\AddTaskControllerFactory();
     $container['CompleteTaskController'] = new \Portal\Factories\CompleteTaskControllerFactory();
+    $container['RandomTaskController'] = new \Portal\Factories\RandomTaskControllerFactory();
 };
